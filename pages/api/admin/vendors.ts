a0b125c1cf8c53b61f36f3b33b60948ede7d980e
Gigabyte00 <topics_slabs_2a@icloud.com>
2025-06-10 03:57:58 -0400

    Add merchant onboarding wizard

---
+++ pages/api/admin/vendors.ts
@@ -0,0 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+import { withRole } from '../../../lib/withRole';
+
+const prisma = new PrismaClient();
+
+export default withRole('ADMIN', async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'GET') {
+    res.setHeader('Allow', 'GET');
+    return res.status(405).end('Method Not Allowed');
+  }
+
+  const vendors = await prisma.vendor.findMany();
+  res.json(vendors);
+});
